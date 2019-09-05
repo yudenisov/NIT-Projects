1138a28862a532e8b76bc2d5d85e70e99bd70b37
Юрий А. Денисов <yudenisov@mail.ru>
2019-09-05 08:32:54 +0400

    Version 1.0.3.0_beta
    - (Add) Documentation at PDF and CHM format
    - (Modify) Order of download to renew curl and wget versions
    - (Fix) Addition modules is not downloaded

--- bin/choc_pack.install.cmd
+++ bin/choc_pack.install.cmd
@@ -7,11 +7,13 @@ rem Check if Chocolatey is Installed
 if not exist %Chocolatey% goto ch_not_installed
 echo "Install Chocolatey Packets..."
 %Chocolatey%\bin\choco.exe install -y --force --ignore-checksums sysinternals
+%Chocolatey%\bin\choco.exe install -y --force --ignore-checksums duck
+%Chocolatey%\bin\choco.exe install -y --force --ignore-checksums 7Zip.install
+%Chocolatey%\bin\choco.exe install -y --force --ignore-checksums 7zip
+%Chocolatey%\bin\choco.exe install -y --force --ignore-checksums 7zip.portable
+%Chocolatey%\bin\choco.exe install -y --force --ignore-checksums unxutils
 %Chocolatey%\bin\choco.exe install -y --force --ignore-checksums curl
 %Chocolatey%\bin\choco.exe install -y --force --ignore-checksums wget
-%Chocolatey%\bin\choco.exe install -y --force --ignore-checksums duck
-%Chocolatey%\bin\choco.exe install -y --force --ignore-checksums 7Zip.install 7zip 7zip.portable
-%Chocolatey%\bin\choco.exe install -y --force --ignore-checksums unxutils
 rem
 %Chocolatey%\bin\RefreshEnv.cmd
 echo "Success! Chocolatey Packets have been installed."
